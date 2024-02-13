09e1388ecacb969d28ac4d3b98d10be06c52bdbc
Junjie Fang <fang.junj@northeastern.edu>
2024-02-13 12:48:38 -0800

    initial app

---
+++ app/src/main/java/edu/northeastern/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.northeastern.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        int theId = view.getId();
+    }
+}
