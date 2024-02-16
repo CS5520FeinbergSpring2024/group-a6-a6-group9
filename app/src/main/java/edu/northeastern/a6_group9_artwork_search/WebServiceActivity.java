a70c9a3eeebc5e0bda28b4520a07df955227f37a
BinZ <crystal622114@gmail.com>
2024-02-16 12:05:36 -0800

    Replaced DatePicker with NumberPicker

--- app/src/main/java/edu/northeastern/a6_group9_artwork_search/WebServiceActivity.java
+++ app/src/main/java/edu/northeastern/a6_group9_artwork_search/WebServiceActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -13,7 +14,7 @@ import java.util.Calendar;
 public class WebServiceActivity extends AppCompatActivity {
     private EditText titleEditText;
     private EditText artistEditText;
-    private DatePicker yearPicker;
+    private NumberPicker yearPicker;
     private String[] userInputs = new String[3];  //usersInput[0] is title, [1] is artist, [2] is year
 
     @Override
@@ -25,22 +26,17 @@ public class WebServiceActivity extends AppCompatActivity {
         artistEditText = (EditText) findViewById(R.id.et_artist);
 
         // Initialize the DatePicker
-        yearPicker = (DatePicker) findViewById(R.id.yearPicker);
-        // Set the initial value
-        Calendar calendar = Calendar.getInstance();
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-        yearPicker.init(currentYear, currentMonth, currentDay, null);
+        yearPicker = (NumberPicker) findViewById(R.id.yearPicker);
+        yearPicker.setMaxValue(2024);
+        yearPicker.setMinValue(1700);
+        yearPicker.setValue(2024);
 
-        // Set OnDateChangedListener to handle the selected year
-        yearPicker.setOnDateChangedListener((view, year, monthOfYear, dayOfMonth) ->
-                userInputs[2] = String.valueOf(year));
     }
 
     public void callWebserviceButtonHandler(View view){
         userInputs[0] = titleEditText.getText().toString().trim();
         userInputs[1] = artistEditText.getText().toString().trim();
+        userInputs[2] = String.valueOf(yearPicker.getValue());
         //Toast.makeText(this, "userInputs: " + userInputs[0] +
                 //", " + userInputs[1] + ", " + userInputs[2], Toast.LENGTH_LONG).show();
 
