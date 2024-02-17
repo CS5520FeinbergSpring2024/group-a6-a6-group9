8d28bc4a5717b7ac019eb0d83833fd6a352d743f
Xiran Jia <jiaxiran98@gmail.com>
2024-02-16 16:19:44 -0800

    implement search api
    Change-Id: If7e417ddfd37ec414314b411ab147f1ebc376de3

--- app/src/main/java/edu/northeastern/a6_group9_artwork_search/at_your_service/ArtICClient.java
+++ app/src/main/java/edu/northeastern/a6_group9_artwork_search/at_your_service/ArtICClient.java
@@ -12,6 +12,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ArtICClient {
@@ -27,22 +28,23 @@ public class ArtICClient {
      */
     public ListResponse listArtwork(int page) {
         JSONObject resp = queryResponse(formatURL("artworks", artworksFields, page, ""));
-        return new ListResponse(getPagination(resp), Artwork.class.getSimpleName(), getArtworks(resp));
+        return new ListResponse(getPagination(resp), getArtworks(resp));
     }
 
     /**
      *
      * @param page page number
-     * @param fullTextContains will be used in a full text search on all metadata
-     * @param titleContains will be searched for as a substring in title
-     * @param completeYearGt will be compared with the complete year, keep whose complete year is greater than the given number
-     * @param completeYearLt will be compared with the complete year, keep whose complete year is lower than the given number
-     * @param artistContains will be searched for as a substring in artist
+     * @param fullTextContains will be used in a full text search on all metadata, case in-sensitive
+     * @param titleContains will be searched for as a substring in title, case in-sensitive
+     * @param completeYearGte will be compared with the complete year, keep whose complete year is greater than or equal to the given number
+     * @param completeYearLte will be compared with the complete year, keep whose complete year is lower than or equal to the given number
+     * @param artistContains will be searched for as a substring in artist, case in-sensitive
      *
      * @return may be empty
      */
-    public ListResponse listArtwork(int page, String fullTextContains, String titleContains, int completeYearGt, int completeYearLt, String artistContains) {
-        return null;
+    public ListResponse listArtwork(int page, String fullTextContains, String titleContains, int completeYearGte, int completeYearLte, String artistContains) {
+        JSONObject resp = queryResponse(formatURL("artworks", artworksFields, page, formatArtworkQueryParams(fullTextContains, titleContains, completeYearGte, completeYearLte, artistContains)));
+        return new ListResponse(getPagination(resp), getArtworks(resp));
     }
 
     private Pagination getPagination(JSONObject obj) {
@@ -112,28 +114,73 @@ public class ArtICClient {
      *
      * @param resourceName one of the <a href="https://api.artic.edu/docs/#collections-2">artic collections</a>, e.g. artworks, agents
      * @param fields the fields that need to be kept
-     * @param page page number
-     * @param fullTextQuery the full text search pattern that will be used in q, can be empty
      */
-    private URL formatURL(String resourceName, String[] fields, int page, String fullTextQuery) {
+    private URL formatURL(String resourceName, String[] fields, int page, String queryParams) {
         StringBuilder urlStringBuilder = new StringBuilder("https://api.artic.edu/api/v1/");
         urlStringBuilder.append(resourceName).append("/search?page=").append(page).append("&fields=").append(String.join(",", fields));
         try {
-            if (fullTextQuery != null && !fullTextQuery.isEmpty()) {
-                urlStringBuilder.append("&params=").append(URLEncoder.encode(String.format("{\"q\":\"%s\"}", fullTextQuery), "UTF-8"));
-            }
+            urlStringBuilder.append("&params=").append(URLEncoder.encode(queryParams, "UTF-8"));
         } catch (UnsupportedEncodingException e) {
-            Log.e(logTag, "UnsupportedEncodingException: " + fullTextQuery);
+            Log.e(logTag, "UnsupportedEncodingException: " + queryParams);
         }
         URL url = null;
         String urlString = urlStringBuilder.toString();
-        Log.d(logTag, "urlString: " + urlString);
         try {
             url = new URL(urlString);
         } catch (MalformedURLException e) {
             Log.e(logTag, "MalformedURLException: " + urlString);
         }
         return url;
+    }
+
+    private String formatArtworkQueryParams(String fullTextContains, String titleContains, int completeYearGte, int completeYearLte, String artistContains) {
+        JSONObject params = new JSONObject();
+        try {
+            if (!fullTextContains.isEmpty()) {
+                params.put("q", fullTextContains);
+            }
+
+            JSONObject query = new JSONObject();
+
+            if (!titleContains.isEmpty() || !artistContains.isEmpty()) {
+                JSONObject matchQuery = new JSONObject();
+                query.put("match", matchQuery);
+
+                if (!titleContains.isEmpty()) {
+                    JSONObject titleQuery = new JSONObject();
+                    matchQuery.put("title", titleQuery);
+                    titleQuery.put("query", titleContains);
+                }
+
+                if (!artistContains.isEmpty()) {
+                    JSONObject artistQuery = new JSONObject();
+                    matchQuery.put("artist_title", artistQuery);
+                    artistQuery.put("query", artistContains);
+                }
+            }
+
+            if (completeYearGte > 0 || completeYearLte > 0) {
+                JSONObject rangeQuery = new JSONObject();
+                query.put("range", rangeQuery);
+
+                JSONObject completeYearQuery = new JSONObject();
+                rangeQuery.put("date_end", completeYearQuery);
+
+                if (completeYearGte > 0) {
+                    completeYearQuery.put("gte", completeYearGte);
+                }
+                if (completeYearLte > 0) {
+                    completeYearQuery.put("lte", completeYearLte);
+                }
+            }
+
+            if (query.length() > 0) {
+                params.put("query", query);
+            }
+        } catch (JSONException e) {
+            Log.e(logTag, String.format(Locale.getDefault(), "JSONException, fullTextContains: %s, titleContains: %s, completeYearGt: %d, completeYearLt: %d, artistContains: %s", fullTextContains, titleContains, completeYearGte, completeYearLte, artistContains));
+        }
+        return params.toString();
     }
 
     private String convertStreamToString(InputStream is) {
