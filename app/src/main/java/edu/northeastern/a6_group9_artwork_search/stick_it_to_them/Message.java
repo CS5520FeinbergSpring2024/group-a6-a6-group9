601ad8be17c97295bff89bb9bb44d333fed5b9ac
Xiran Jia <jiaxiran98@gmail.com>
2024-02-28 18:41:56 -0800

    messages completed
    Change-Id: I7053b4f7bc0c45df356018c0829626af9bccab03

---
+++ app/src/main/java/edu/northeastern/a6_group9_artwork_search/stick_it_to_them/Message.java
@@ -0,0 +1,40 @@
+package edu.northeastern.a6_group9_artwork_search.stick_it_to_them;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@IgnoreExtraProperties
+public class Message {
+    private String senderUsername;
+    private String receiverUsername;
+    private String stickerId;
+    private Date sendTime;
+
+    // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    public Message() {}
+
+    public Message(String senderUsername, String receiverUsername, String stickerId) {
+        this.senderUsername = senderUsername;
+        this.receiverUsername = receiverUsername;
+        this.stickerId = stickerId;
+        sendTime = Calendar.getInstance().getTime();
+    }
+
+    public String getSenderUsername() {
+        return senderUsername;
+    }
+
+    public String getReceiverUsername() {
+        return receiverUsername;
+    }
+
+    public String getStickerId() {
+        return stickerId;
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+}
--- app/src/main/java/edu/northeastern/a6_group9_artwork_search/stick_it_to_them/User.java
+++ app/src/main/java/edu/northeastern/a6_group9_artwork_search/stick_it_to_them/User.java
@@ -1,9 +1,12 @@
 package edu.northeastern.a6_group9_artwork_search.stick_it_to_them;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import androidx.annotation.NonNull;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
-public class User {
+public class User implements Parcelable {
     private String username;
 
     // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -13,7 +16,33 @@ public class User {
         this.username = username;
     }
 
+    protected User(Parcel in) {
+        username = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(username);
     }
 }
